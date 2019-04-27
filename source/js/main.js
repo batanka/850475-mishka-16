4d33e2802a975a2402ce59e2da7e603ea792bfbe
batanka <k.gulena@gmail.com>
2019-04-27 21:11:10 +0300

    Продолжение мобильного меню.

--- source/js/main.js
+++ source/js/main.js
@@ -43,21 +43,3 @@ closeBtn.addEventListener("keydown", function (evt) {
     }
   }
 });
-
-//Мобильное меню
-var mainNav = document.querySelector(".main-nav");
-var toggleNav = mainNav.querySelector(".main-nav__toggle");
-
-mainNav.classList.remove("main-nav--no-js");
-
-toggleNav.addEventListener ("click", function (evt) {
-  evt.preventDefault();
-  if (mainNav.classList.contains("main-nav--closed")) {
-    mainNav.classList.add("main-nav--opened");
-    mainNav.classList.remove("main-nav--closed");
-  }
-  else {
-    mainNav.classList.add("main-nav--closed");
-    mainNav.classList.remove("main-nav--opened");
-  }
-});
